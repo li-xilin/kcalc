8abc8749c3899ca81d8e33f4c490928dd1375b84
Li Xilin <lixilin@gmx.com>
2023-11-18 21:58:42 +0800

    feat: add usage

--- main.c
+++ main.c
@@ -33,16 +33,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-ax_iobuf sg_expbuf;
-extern int result_base;
-extern ax_u1024 result_value;
-
 int yyparse();
 int yywrap();
 int yyrestart(FILE *f);
-extern int yychar;
-extern YYSTYPE yylval;
-extern int yynerrs;
+
+static const char usage[] =
+	"KCALC - Calculator for 1024-bit large integer "
+	"\n"
+	"\nTokens: "
+	"\n    PRINT D E F G H I J K"
+	"\nOperators:"
+	"\n    ~, ^, *, /, %, +, -, <<, >>, and, xor, or"
+	"\nLiteral value:"
+	"\n    1. Hexadecimal integer prefix with 0x. (eg. 0x2Aef)"
+	"\n    2. Decimal integer. (eg. 042)"
+	"\nFunctions:"
+	"\n    1. pow(x, p): x to the power of p."
+	"\n    2. sqrt(x): square root of x."
+	"\nPrint result:"
+	"\n    1. 'x' for hexadecimal value output, default behavior."
+	"\n    2. 'd'  for decimal value output."
+	"\nGrammar:"
+	"\n    EVAL -> x EXP | d EXP | EXP"
+	"\n    EXP -> EXP or E | E"
+	"\n    E -> E xor F | F"
+	"\n    F -> F and G | G"
+	"\n    G -> G << H | G >> H | H"
+	"\n    H -> H + I | H - I | I"
+	"\n    I -> I * J | I / J | I % J | J"
+	"\n    J -> K ^ J | K"
+	"\n    K -> (D) | pow(D,D) | sqrt(D) | <literal value> | ~ K"
+	"\n"
+	"\nCopyright (c) Li Xilin <lixilin@gmx.com>"
+	"\n";
+
+ax_iobuf sg_expbuf;
+extern int result_base;
+extern ax_u1024 result_value;
 
 int lex_input(char *buf, size_t size)
 {
@@ -54,14 +81,6 @@ void yyerror(const char *err)
 	ax_perror(err);
 }
 
-bool is_empty(const char *text)
-{
-	for (int i = 0; text[i]; i++)
-		if (text[i] != '\t' && text[i] != ' ' && text[i] != '\n')
-			return false;
-	return true;
-}
-
 int main()
 {
 	ax_log_set_mode(AX_LM_NOLOC|AX_LM_NOTIME);
@@ -70,15 +89,24 @@ int main()
 		if (!line)
 			break;
 
-		size_t len = strlen(line);
-
-		if (is_empty(line))
+		size_t len;
+		char *exp = ax_strtrim(line, &len);
+		if (exp[0] == '\0')
 			goto cont;
-		if (strcmp(line, "q") == 0)
+		if (strcmp(exp, "q") == 0)
 			break;
-		ax_edit_history_add(line);
 
-		ax_iobuf_init(&sg_expbuf, line, len + 1);
+		if (strcmp(exp, "quit") == 0)
+			break;
+
+		ax_edit_history_add(exp);
+
+		if (strcmp(exp, "help") == 0) {
+			fputs(usage, stdout);
+			goto cont;
+		}
+
+		ax_iobuf_init(&sg_expbuf, exp, len + 1);
 		sg_expbuf.rear = len;
 		if (yyparse())
 			goto cont;
